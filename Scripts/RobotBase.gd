657ef13a143ce302ad237aa87a66139d8ee1e57a
pielordzz <zanezmola@gmail.com>
2024-08-17 18:35:41 -0400

    tower ai made funtional

--- Scripts/RobotBase.gd
+++ Scripts/RobotBase.gd
@@ -1,19 +1,16 @@
 extends Node
 
-var projectile
+var projectile = preload('res://assets/Projectile.tscn')
 var rangeRadius
 var damage = 5
 var pathName
 var targets = []
 var activeTarget
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if is_instance_valid(activeTarget):
 		if activeTarget.rotation > PI:
 			if activeTarget.rotation < PI/2 || activeTarget.rotation >= 3*PI/2:
@@ -28,8 +25,9 @@ func _process(delta: float) -> void:
 				$Sprite2D.texture = load("res://assets/temporary/up-right.png")
 
 
-func _EnemyInRange(range):
-	if 'car' in range.name:
+func _on_range_2d_body_entered(body: Node2D) -> void:
+	if 'car' in body.name:
+		$ShotTimer.start()
 		var tempArray = []
 		#gets all nodes with collision in range
 		targets = get_node('Range2D').get_overlapping_bodies()
@@ -51,13 +49,30 @@ func _EnemyInRange(range):
 					
 		
 		pathName = activeTarget.get_parent().name
-		
 		#spawns projectile toward enemy
+
+func _on_range_2d_body_exited(body: Node2D) -> void:
+	print('exit')
+	if 'car' in body.name:
+		targets = get_node('Range2D').get_overlapping_bodies()
+		activeTarget = null
+		var tempArray = []
+		for i in targets:
+			if 'car' in i.name:
+				tempArray.append(i)
+
+		
+
+		
+		
+		
+		
+func _on_timer_timeout() -> void:
+	if activeTarget != null:
 		var tempProjectile = projectile.instantiate()
 		tempProjectile.pathName = pathName
 		tempProjectile.damage = damage
+		await get_tree().process_frame
 		get_node('ProjectileDisconect').add_child(tempProjectile)
-		tempProjectile.globalPosition = $Aim.global_position
-		
-		
-		
+		tempProjectile.target = activeTarget
+		tempProjectile.global_position = $TowerArea.global_position
