70143ec16de0e32d0ea690bf8a9b1e953627c527
Alex Pratte <alexpratte16@gmail.com>
2024-08-17 23:28:02 -0400

    Basic level framework

--- Scripts/game_ui.gd
+++ Scripts/game_ui.gd
@@ -4,15 +4,16 @@ extends Control
 @export var game_texture: TextureRect
 @export var game_viewport: SubViewport
 
-@export var game_scene: Node2D
+@export var game_scene: TDLevel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var mpos: Vector2 = game_viewport.get_mouse_position()
-	
 	game_scene.update_pointer_position(mpos)
+	
+	var levelLabel : Label = get_node("MainPanel/VerticalContainer/Top Bar/Level")
+	levelLabel.text = "Level: " + str(game_scene.get_level())
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -9,26 +9,53 @@ class_name TDLevel
 @export var db_dot: Sprite2D
 
 var score
+var level = 0
+var current_wave = []
 
+func get_level():
+	return level
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$MobTimer.start()
 
+func wave_factory(level: int) -> Array:
+	#TODO: figure out wave loading for actually generating waves
+	var ret = []
+	for level_idx in range(level):
+		var health_comp = HealthComponent.new(1, 1)
+		var car = car_factory(health_comp)
+		ret.append(car)
+	return ret
 
+func car_factory(health_comp: HealthComponent) -> Car:
+	var car: Car = mob_scene.instantiate()
+	var health_component: HealthComponent = health_comp
+	car.initilize(health_component)
+
+	return car
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-	
+
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
 	var path = mob_path.instantiate()
-	var car: Car = mob_scene.instantiate()
-	var health_component: HealthComponent = HealthComponent.new(3, 5)
-	car.initilize(health_component)
-	path.get_child(0).add_child(car)
-	add_child(path)
+
+	# Current wave is empty
+	if current_wave == [] and path.get_child(0).get_child_count() == 0:
+		level+=1
+		current_wave = wave_factory(level)
+		print(level)
+
+	# There are cars left in the current wave
+	else:
+		# Get new car and add to scene
+		var car = current_wave.pop_front()
+		path.get_child(0).add_child(car)
+		add_child(path)
+
 
 #this is for debugging
 func update_pointer_position(pos: Vector2):
