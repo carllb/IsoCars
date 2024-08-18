b0520f8a8761bff7cdd45aaeb868a672b7534fcb
pielordzz <zanezmola@gmail.com>
2024-08-18 17:58:48 -0400

    burn, slow and other towers are functional

--- Scripts/car.gd
+++ Scripts/car.gd
@@ -37,9 +37,12 @@ func initilize(health_component: HealthComponent,
 	car_death.connect(on_death_cb)
 
 func take_damage(damage: float, _damage_type: String = 'PHYSICAL') -> void:
-	if _damage_type == "ICE":
+	if (_damage_type == "ICE")&&damage>0:
 		$SlowDownTimer.start()
 		speed.apply_slow(damage)
+	elif (_damage_type == "FIRE")&&damage>0:
+		health.burn(damage)
+		$BurnTimer.start
 	else:
 		health.take_damage(damage)
 	
