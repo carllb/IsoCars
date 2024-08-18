62ed6ebe9de712b723b277b107a04e6058cfa4fc
James <jamesman45@gmail.com>
2024-08-17 20:48:00 -0400

    feat: adding health component

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -18,5 +18,10 @@ func _process(_delta: float) -> void:
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
 	var path = mob_path.instantiate()
+	var car: Car = mob_scene.instantiate()
+	var health_component: HealthComponent = HealthComponent.new()
+	health_component.armor = 50
+	car.health = health_component
+	path.get_child(0).add_child(car)
 	add_child(path)
 	
