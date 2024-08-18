a78f82772ceb4b2eba96f8364673c3a820632cf7
pielordzz <zanezmola@gmail.com>
2024-08-18 15:01:46 -0400

    fixed target issue

--- Scripts/RobotBase.gd
+++ Scripts/RobotBase.gd
@@ -29,54 +29,51 @@ func _process(_delta: float) -> void:
 
 
 func _on_range_2d_body_entered(body: Node2D) -> void:
-	if 'Car' in body.name:
-		$ShotTimer.start()
-		
-		var tempArray = []
-		#gets all nodes with collision in range
-		targets = get_node('Range2D').get_overlapping_bodies()
-		#gets cars in range
-		for i in targets:
-			if 'Car' in i.name:
-				tempArray.append(i)
-				
-		activeTarget = null
-		
-		#sets target to most progress
-		for i in tempArray:
-			if activeTarget == null:
-				activeTarget = i.get_node('../')
-			else:
-				if i.get_parent().get_progress() > activeTarget.get_progress():
-					activeTarget = i.get_node('../')
-					
-		pathName = activeTarget.get_parent().name
-		#spawns projectile toward enemy
+	if "Car" in body.name:
+		find_target(body)
 
 func _on_range_2d_body_exited(body: Node2D) -> void:
-	if 'Car' in body.name:
-		targets = get_node('Range2D').get_overlapping_bodies()
-		activeTarget = null
-		var tempArray = []
-		for i in targets:
-			if 'Car' in i.name:
-				tempArray.append(i)
-		#sets target to most progress
-		for i in tempArray:
-			if activeTarget == null:
+	print(body)
+	find_target(body)
+
+func find_target(body: Node2D):
+
+	$ShotTimer.start()
+	
+	var tempArray = []
+	#gets all nodes with collision in range
+	targets = get_node('Range2D').get_overlapping_bodies()
+	#gets cars in range
+	for i in targets:
+		if 'Car' in i.name:
+			tempArray.append(i)
+			
+	activeTarget = null
+	
+	#sets target to most progress
+	for i in tempArray:
+		if activeTarget == null:
+			activeTarget = i.get_node('../')
+			pathName = activeTarget.get_parent().name
+		else:
+			if i.get_parent().get_progress() > activeTarget.get_progress():
 				activeTarget = i.get_node('../')
-			else:
-				if i.get_parent().get_progress() > activeTarget.get_progress():
-					activeTarget = i.get_node('../')
+				pathName = activeTarget.get_parent().name
+		
+
 
 func _on_timer_timeout() -> void:
 	if activeTarget != null:
 		var tempProjectile = projectile.instantiate()
-		tempProjectile.initilize(400, damage_array,activeTarget, pathName)
+		tempProjectile.initilize(250, damage_array,activeTarget, pathName)
 		await get_tree().process_frame
 		get_node('ProjectileDisconect').add_child(tempProjectile)
-		tempProjectile.target = activeTarget
 		tempProjectile.global_position = $TowerArea.global_position
+	else:
+		find_target($Range2D)
+		if activeTarget == null:
+			$ShotTimer.stop()
+
 
 func get_sprite() -> Sprite2D:
 	return get_node("Sprite2D")
