ac5de3d9ae38b877ddbbde85fcc64864ca731c35
pielordzz <zanezmola@gmail.com>
2024-08-18 23:31:59 -0400

    upgrade logic made --todo limit upgrades and enforce cost

---
+++ Scripts/damage_phys_upgrade.gd
@@ -0,0 +1,6 @@
+extends "res://Scripts/upgrade_button.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _pressed():
+	linked_tower.set_damage([linked_tower.get_damage()[0] +5 ,0,0])
---
+++ Scripts/fire_rate_upgrade.gd
@@ -0,0 +1,6 @@
+extends "res://Scripts/upgrade_button.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _pressed():
+	linked_tower.set_fire_rate(linked_tower.get_fire_rate()*1.25)
