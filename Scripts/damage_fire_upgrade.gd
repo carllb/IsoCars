bb846871b4c2272ee614d6e600caa2111cc3b88e
pielordzz <zanezmola@gmail.com>
2024-08-19 21:08:19 -0400

    time between cars decreased and start game button

--- Scripts/damage_fire_upgrade.gd
+++ Scripts/damage_fire_upgrade.gd
@@ -3,5 +3,5 @@ extends "res://Scripts/upgrade_button.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _pressed():
-	linked_tower.set_damage([linked_tower.get_damage()[0],linked_tower.get_damage()[1]+3,linked_tower.get_damage()[2]],true)
+	linked_tower.set_damage([linked_tower.get_damage()[0],linked_tower.get_damage()[1]+2,linked_tower.get_damage()[2]],true)
 	purchase()
